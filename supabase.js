f4b162a52dd64aa7bd892761ce12d3718b4f75bc
fahri <150719442+fahrihostingg@users.noreply.github.com>
2026-05-03 22:26:26 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,11 +1,11 @@
 // GANTI 2 BARIS INI DENGAN PROJECT KAMU
-/*
+
 const SUPABASE_URL = 'https://gcjoxanfwnmdrphnfbjn.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdjam94YW5md25tZHJwaG5mYmpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3MDUyMTUsImV4cCI6MjA5MzI4MTIxNX0.uuWhaeuPnvXKbmAv6zN825y2EKiaQ84k8-aDtZWMbsQ';
-*/
+/*
 const supabaseUrl = 'https://gcjoxanfwnmdrphnfbjn.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdjam94YW5md25tZHJwaG5mYmpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3MDUyMTUsImV4cCI6MjA5MzI4MTIxNX0.uuWhaeuPnvXKbmAv6zN825y2EKiaQ84k8-aDtZWMbsQ';
-
+*/
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 function initTheme() {
